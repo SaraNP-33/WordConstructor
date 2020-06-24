503a233cc75fd554c68a76e131ff6fdd62440199
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-06-24 09:19:07 -0700

    word object and main game functions done

---
+++ index.js
@@ -0,0 +1,150 @@
+var inquirer =require("inquirer");
+var colors =require("colors");
+var word =require("./Words.js");
+
+var randomWord;
+var remainingGuesses= 9;
+var lettersGuessed=[];
+
+var plants=[
+    "Elephant",
+    "Black Bear",
+    "Barracuda",
+    "Bearded Dragon",
+    "Beaver",
+    "Cheetah",
+    "Dolphin",
+    "Bumblee Bee",
+    "Honey Bee",
+    "Portuguese Water Dog",
+    "Komodo Dragon",
+    "Gila Monster",
+    "Ratlesnake",
+    "Black Mamba",
+    "Poison Dart Frog",
+    "Platypus",
+    "Porcupine",
+    "Royal Penguin",
+    "Puffer Fish",
+    "Mongoose",
+    "Ostrich",
+    "Ocelot",
+    "Old English Sheepdog",
+    "Opossum",
+    "Kangaroo",
+    "King Crab",
+    "Jellyfish",
+    "Japanese Macaque",
+    "Pygmy Hippopotamus",
+    "Nightingale"
+]
+
+//function to Start Game
+
+function startGame(){
+    inquirer
+    .prompt([
+        {
+        name:"start game",
+        type:"confirm",
+        message:"Are you ready to test your animal knowledge?".blue
+        }
+    ]).then(function(answer){
+        remainingGuesses=9;
+        lettersGuessed=[];
+        console.log("Guess the Animal!".green)
+        if(plants.length >0){
+            var random =Math.floor(Math.random()* plants.length);
+            randomWord =plants[random];
+            plants.splice(random, 1);
+            // console.log(plants)
+            // console.log(randomWord);
+
+
+            //using word constructor to turn the plant into a word object
+            randomWord = new Word(randomWord);
+            
+            // underscores for user to know how many letters
+            console.log(randomWord.display());
+            userGuess();
+        } else{
+            console.log("You know your Animals!!".yellow,"There are no more Animals to guess".red)
+        }
+    })
+
+    //function to allow user to guess a word
+
+    function userGuess(){
+        inquirer
+        .prompt([{
+            name:"userGuess",
+            type: "Input",
+            message:"Guess a letter: ".magenta
+
+        }]).then(function(answer){
+            //creatin variable to use it more easily down the function that turns all letters to upper case letters
+            var userGuess = answer.userGuess.toUpperCase();
+
+            //tell the user if a letter has been already used
+            if(lettersGuessed.includes(userInput)){
+                console.log("You already guessed the letter", userGuess.toUpperCase().bgYellow,". Try another!");
+                userGuess();
+            } else{
+                //add the letter the user guessed to the array
+                lettersGuessed.push(userGuess);
+
+                //check if the letter is right - this uses the check from the word constructor that uses it from letter.js
+                randomWord.check(userGuess)
+
+                //if the letter is correct, show it on the console
+                var show = randomWord.display();
+                console.log(show);
+
+                //if the user guesses wrong
+                if(!show.includes(userGuess)){
+                    remainingGuesses--;
+                
+                //keep track of the number of guesses left
+                console.log("Wrong Letter".bgRed,"you have", remainingGuesses.bgYellow, "guesses left");
+                
+                }else{
+                    console.log("You Got It!".bgGreen);
+                }
+                //if the user wins
+                if(!show.includes("_")){
+                    console.log("Woohoo! You won!".bgGreen);
+                }
+                else if(remainingGuesses>0){
+                    userGuess();
+                }
+                else{
+                    //user lost
+                    console.log("Boo...you lost! Try again!".bgCyan);
+                    restart();
+                }
+            }
+        })
+    }
+
+
+   //function to restart game
+
+   function restart(){
+       inquirer
+       .prompt([{
+           name:"restart",
+           type:"confirm",
+           message:"Would you like to give it another try?".brightCyan
+       }]).then(function(answer){
+           if(answer.restart){
+               startGame();
+           }else{
+               console.log("It has been real!".brightBlue, "Thanks for playing!".brightRed, "See you soon!".brightWhite);
+               
+           }
+       })
+   }
+
+}
+
+startGame();
